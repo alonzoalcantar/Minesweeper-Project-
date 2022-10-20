bbe2d636b3b3037c26037dd1ad332c410e1c48cb
alonzoalcantar <pinoa.03@outlook.com>
2022-10-20 01:05:06 -0700

    trying to count number of mines near square

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,7 +6,7 @@ let squares = [];
 let gameIsActive = true;
 let plantingFlag = false;
 let minesInGame = 10;
-
+let i = 0;
 
 //Constant variables
 
@@ -21,7 +21,7 @@ const clearSquares = Array(8*8 - minesInGame).fill("clear");
 const boardArray = clearSquares.concat(mines);
 const randomizeMine = boardArray.sort(() => Math.random() -0.5);
 const boardLeft = (i % 8 === 0);
-const boardRight = (i % 8 === -1);
+const boardRight = (i % 8 === 8 - 1);
 
 //Text Variables
 const hiddenSquare = "Hidden";
@@ -43,7 +43,17 @@ const createBoard = () => {
         board.appendChild(square);
         squares.push(square);
     }
+    //adds numbers to clear squares for amount of mines nearby based on position
+    for (i = 0; i < squares.length; i++) {
+        let mineTotal = 0;
+        if (squares[i].classList.contains("clear")){
+            if (i > 0 && !boardLeft && squares[i -1].classList.contains("mine")) mineTotal ++;
+            if (i > 7 && !boardRight && squares[i +1 -8].classList.contains("mine")) mineTotal ++;
 
+
+            squares[i].setAttribute("minesNearby", mineTotal);
+    }
+}
 
 }
 createBoard();
@@ -60,7 +70,8 @@ createBoard();
 
 
 //Test constant variable values 
-console.log(clearSquares);
-console.log(mines);
-console.log(boardArray);
-console.log(randomizeMine);
+// console.log(clearSquares);
+// console.log(mines);
+// console.log(boardArray);
+// console.log(randomizeMine);
+console.log(squares[i]);
