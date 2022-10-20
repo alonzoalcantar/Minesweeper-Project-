a9e0c18a895a1b9521da0edf182e6dac2d311185
alonzoalcantar <pinoa.03@outlook.com>
2022-10-19 22:44:06 -0700

    defined mines & squares as own arrays to refrence

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,32 +1,32 @@
 console.log("hello")
-
+let minesInGame = 10;
 //Constant variables
-const squares = Array.from(document.querySelectorAll(".square"));
+//const squares = Array.from(document.querySelectorAll(".square"));
 const resetButton = document.querySelector("#reset");
 const timeElapsed = document.querySelector(".timer");
-const board = document.querySelector(".board")
+
+
+const board = document.querySelector(".board");
+const mines = Array(minesInGame).fill("mine");
+const emptySquares = Array(8*8 - minesInGame).fill("clear");
 
 const hiddenSquare = "Hidden";
 const mineOnSquare = "Mine On Square";
 const mineNearSquare = "Number On Square";
 const flaggedSquare = "Flag on Square";
 
-const setAttribute = () => document.createAttribute("Mine");
+
+
 
 //What will change in the game
-// let board = 
-// ["","","","","","","","",
-// "","","","","","","","",
-// "","","","","","","","",
-// "","","","","","","","",
-// "","","","","","","","",
-// "","","","","","","","",
-// "","","","","","","","",
-// "","","","","","","","",];
+let squares = [];
+
+
 
 let gameIsActive = true;
 let plantingFlag = false;
 
+//let minesInGame = 10;
 
 
 
@@ -37,15 +37,18 @@ const createBoard = () => {
         const square = document.createElement("div")
         square.setAttribute("empty", i)
         board.appendChild(square)
+        squares.push(square)
     }
 }
+createBoard();
+
+console.log(emptySquares)
+console.log(mines)
 
 
 
 
-
-
-//Function to randomize mine placement on board array
+//Function to randomize mine placement on squares array
 
 
 
