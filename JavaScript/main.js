19e4c5b11e734b3a2552eb7b4004304f72d34d9d
alonzoalcantar <pinoa.03@outlook.com>
2022-10-20 00:23:27 -0700

    defined edge of board

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,4 @@
-console.log("hello")
+console.log("hello");
 
 
 //What will change in the game
@@ -8,21 +8,22 @@ let plantingFlag = false;
 let minesInGame = 10;
 
 
-
 //Constant variables
-//const squares = Array.from(document.querySelectorAll(".square"));
 
-
+//Display Variables
 const resetButton = document.querySelector("#reset");
 const timeElapsed = document.querySelector(".timer");
 
-
+//Board Variables
 const board = document.querySelector(".board");
 const mines = Array(minesInGame).fill("mine");
 const clearSquares = Array(8*8 - minesInGame).fill("clear");
 const boardArray = clearSquares.concat(mines);
-const randomizeMine = boardArray.sort(() => Math.random() -0.5)
+const randomizeMine = boardArray.sort(() => Math.random() -0.5);
+const boardLeft = (i % 8 === 0);
+const boardRight = (i % 8 === -1);
 
+//Text Variables
 const hiddenSquare = "Hidden";
 const mineOnSquare = "Mine On Square";
 const mineNearSquare = "Number On Square";
@@ -34,20 +35,22 @@ const flaggedSquare = "Flag on Square";
 //Function to create board 
 
 const createBoard = () => {
-    for(i = 0; i < 8*8; i++){
+    //creates divs and randomizes mine placement on board
+    for (i = 0; i < 8*8; i++){
         const square = document.createElement("div");
         square.setAttribute("empty", i);
         square.classList.add(randomizeMine[i]);
         board.appendChild(square);
         squares.push(square);
     }
+
+
 }
 createBoard();
 
 
 
 
-//Function to randomize mine placement on squares array
 
 
 
@@ -56,6 +59,7 @@ createBoard();
 
 
 
+//Test constant variable values 
 console.log(clearSquares);
 console.log(mines);
 console.log(boardArray);
